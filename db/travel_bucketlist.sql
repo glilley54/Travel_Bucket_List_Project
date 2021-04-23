1445801cc2c342e8b15c2b5df11949f9509e74d6
glilley54 <glilley54@gmail.com>
2021-04-23 20:04:03 +0100

    added schema isn sql but doesn not work properly

---
+++ db/travel_bucketlist.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS visits;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS cities;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  countries_visited INT,
+  cities_visited INT
+);
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    visited BOOLEAN
+
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    visited BOOLEAN,
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+
+    
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE,
+    city_id INT REFERENCES cities(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE
+
+);
+
+
