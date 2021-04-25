127d2dd57a0a43f3ae1733322df66eaed34fd6e4
glilley54 <glilley54@gmail.com>
2021-04-25 15:29:52 +0100

    working on the controller

---
+++ controllers/destination_controller.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from repositories import country_repository
+from repositories import city_repository
+
+from flask import Blueprint
+
+destinations_blueprint = Blueprint("destinations", __name__)
+
+
+@destinations_blueprint.route("/destinations")
+def cities ():
+    cities = city_repository.select_all() # NEW
+    return render_template("destinations/index.html", all_cities = cities)
+
+# NEW
+# GET '/destinations/new'
+@destinations_blueprint.route("/destinations/new", methods=['GET'])
+def countries():
+    countries = country_repository.select_all()
+    return render_template("destinations/new.html", all_countries = countries)
+
+
+
+# CREATE
+# POST '/destinations'
+@destinations_blueprint.route("/destinations",  methods=['POST'])
+def create_city():
+    city_name  = request.form['city']
+    country = country_repository.select(request.form['country_id'])
+    city = City(name, country , visited)
+    city_repository.save(city)
+    return redirect('/destinations')
