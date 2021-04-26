9353396718046c37cb22e04b47b924f2a8f51717
glilley54 <glilley54@gmail.com>
2021-04-26 15:33:41 +0100

    updated controller to visit controller and managed to add all destinations to list

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
-from controllers.cities_controller import cities_blueprint
+from flask import Flask, render_template, request, request
+from controllers.visits_controller import visits_blueprint
 
 
 
 app = Flask(__name__)
 
-app.register_blueprint(cities_blueprint)
+app.register_blueprint(visits_blueprint)
 
 @app.route("/")
 def main():
---
+++ controllers/visits_controller.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from repositories import country_repository
+from repositories import city_repository
+
+from models.country import Country
+from models.city import City
+
+from flask import Blueprint
+
+visits_blueprint = Blueprint("visits", __name__)
+
+
+@visits_blueprint.route("/visits")
+def visits ():
+    cities = city_repository.select_all() # NEW
+    return render_template("visits/index.html", all_cities = cities)
+
+# NEW
+# GET '/visits/new'
+#@visits_blueprint.route("/cities/new", methods=['GET'])
+#def new_city():
+    #countries = country_repository.select_all()
+    #return render_template("/visits/new.html", all_countries = countries)
+
+
+
+# CREATE
+# POST '/visits'
+#@visits_blueprint.route("/visits",  methods=['POST'])
+#def create_visit():
+    #city_name  = request.form['city']
+    #country = country_repository.select(request.form['country_id'])
+    #city = City(name, country , visited)
+    #city_repository.save(city)
+    #return redirect('/visits')
